ce02cf434261c0d7fa32c337227ddb448a0ac06b
ainura-t <stredeva@gmail.com>
2021-06-23 17:20:19 -0400

    whatsapp-clone added

---
+++ whatsapp-2/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  
+};
+
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+
+const db = app.firestore();
+const auth = app.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { db, auth, provider };
