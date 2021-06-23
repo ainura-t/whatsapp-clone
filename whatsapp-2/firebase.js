fde96384ab2868ecb24d961fd4543d61c99d6028
ainura-t <stredeva@gmail.com>
2021-06-23 17:24:48 -0400

    firebaseConfig added

--- whatsapp-2/firebase.js
+++ whatsapp-2/firebase.js
@@ -1,7 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  
+  apiKey: "AIzaSyB3FfqVsfG0ZCdzIS7j5Kg6IC7spnSEpIM",
+  authDomain: "whatsapp-2-d8339.firebaseapp.com",
+  projectId: "whatsapp-2-d8339",
+  storageBucket: "whatsapp-2-d8339.appspot.com",
+  messagingSenderId: "21144754375",
+  appId: "1:21144754375:web:7977a0027a94de69db86e2"
 };
 
 const app = !firebase.apps.length
